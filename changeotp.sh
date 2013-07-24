75ac8efe5e399fe11c8fd90c97bfb62ac905bce6
David Sekora <DJSekora@gmail.com>
2013-07-24 16:16:36 +0000

    Last update before migration

---
+++ changeotp.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Update the Tomcat Server with otp changes
+# Must be run as root
+
+# Stop the server
+service tomcat6 stop
+
+# Remove old wars/folders
+rm /usr/share/tomcat6/webapps/opentripplanner-analyst-client.war
+rm /usr/share/tomcat6/webapps/opentripplanner-api-webapp.war
+rm -r -f /usr/share/tomcat6/webapps/opentripplanner-analyst-client
+rm -r -f /usr/share/tomcat6/webapps/opentripplanner-api-webapp
+
+# Add new wars/folders
+cp opentripplanner-analyst-client/target/opentripplanner-analyst-client.war /usr/share/tomcat6/webapps
+cp opentripplanner-api-webapp/target/opentripplanner-api-webapp.war /usr/share/tomcat6/webapps
+
+# Start the server again
+service tomcat6 start
+
+# Exit
+exit
